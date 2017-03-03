0a979fd1652cba3b855992b8d5097f78c55a7fc5
imper <imper@DESKTOP-7HN6DVV>
2017-03-04 01:02:54 +0300

    Add etities: Bus. Order, Ticket, User and Voyage.

---
+++ SheduleVehicles/Domain/Entities/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Ticket
+    {
+        public int TicketId { get; set; }
+        public int OrderId { get; set; }
+        public string FirstNamePassenger { get; set; }
+        public string LastNamePassenger { get; set; }
+        public string DocumentNumberPassenger { get; set; }
+        public int NumberPassenger { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ SheduleVehicles/Domain/Entities/Voyage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Voyage
+    {
+        public int VoyageId { get; set; }
+        public int DepartureId { get; set; }
+        public int ArrivalId { get; set; }
+        public DateTime DepartureDateTime { get; set; }
+        public DateTime ArrivalDataDateTime { get; set; }
+        public string TravelTime { get; set; }
+        public int VoyageNumber { get; set; }
+        public string VoyageName { get; set; }
+        public int NumberSeats { get; set; }
+        public decimal TicketCost { get; set; }
+
+
+    }
+}
