5b789805fe53e6e1b7a385c0286e29a493b849d9
imper <imper@DESKTOP-7HN6DVV>
2017-03-04 03:19:42 +0300

    Added project WebUI, page registration page and a successful registration. Update class User.

--- SheduleVehicles/Domain/Entities/User.cs
+++ SheduleVehicles/Domain/Entities/User.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities
 {
     public class User
-    {
+    {  
+        [Required]
         public int UserId { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public DateTime BirthData { get; set; }
 
     }
---
+++ SheduleVehicles/WebUI/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Entities;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public List<User>Users = new List<User>();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            return View("Registration");
+        }
+
+        [HttpPost]
+        public ActionResult Registration(User user)
+        {
+            return View("RegistrationComplited");
+        }
+    }
+}
